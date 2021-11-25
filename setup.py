eb79dd5a5061f9dec583393de8296df755a5f31a
Dmitry Kalinin <kalinin.mitko@gmail.com>
2021-11-25 23:47:40 +0200

    Revert "Added person"
    This reverts commit 94b71cf99e8e7f92c00f20c164fac0d01d36064e.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="fhir-faker",
+    version="0.0.4",
+    description="A library for generating fake FHIR resources and data types.",
+    long_description="A library for generating fake FHIR resources and data types.",
+    keywords="python, fhir, resources, faker",
+    author="Kalinin Dmitry <kalinin.mitko@gmail.com>",
+    url="https://github.com/null-none/fhir-faker",
+    license="MIT",
+    packages=["src"],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    install_requires=[
+        "Faker==9.8.1",
+    ],
+)
---
+++ src/tests/test_resources.py
@@ -0,0 +1,22 @@
+from src.fake import FakeFHIR
+
+
+class TestResources:
+    def test_organization(self):
+        fake = FakeFHIR()
+        assert "resourceType" in fake.organization().serialize()
+        assert "name" in fake.organization().serialize()
+
+    def test_patient(self):
+        fake = FakeFHIR()
+        assert "resourceType" in fake.patient(sex="male").serialize()
+        assert "name" in fake.patient(sex="male").serialize()
+
+    def test_patient(self):
+        fake = FakeFHIR()
+        assert "resourceType" in fake.practitioner(sex="female").serialize()
+        assert "name" in fake.practitioner(sex="female").serialize()
+
+    def test_appointment(self):
+        fake = FakeFHIR()
+        assert "resourceType" in fake.appointment().serialize()
