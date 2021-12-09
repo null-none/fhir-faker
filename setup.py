e9775f85afdb2a88b4b7b315bd264f143896ec14
Kalinin Dmitry <kalinin.mitko@gmail.com>
2021-12-09 23:16:13 +0200

    Fixed bug in tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fhir-faker",
-    version="0.0.6",
+    version="0.0.7",
     description="A library for generating fake FHIR resources and data types.",
     long_description="A library for generating fake FHIR resources and data types.",
     keywords="python, fhir, resources, faker",
--- src/tests/test_resources.py
+++ src/tests/test_resources.py
@@ -12,7 +12,7 @@ class TestResources:
         assert "resourceType" in fake.patient(sex="male").serialize()
         assert "name" in fake.patient(sex="male").serialize()
 
-    def test_patient(self):
+    def test_practitioner(self):
         fake = FakeFHIR()
         assert "resourceType" in fake.practitioner(sex="female").serialize()
         assert "name" in fake.practitioner(sex="female").serialize()
@@ -24,3 +24,13 @@ class TestResources:
     def test_person(self):
         fake = FakeFHIR()
         assert "resourceType" in fake.person().serialize()
+
+    def test_schedule(self):
+        fake = FakeFHIR()
+        assert "actor" in fake.schedule().serialize()
+
+    def test_slot(self):
+        fake = FakeFHIR()
+        assert "status" in fake.slot().serialize()
+        assert "start" in fake.slot().serialize()
+        assert "end" in fake.slot().serialize()
