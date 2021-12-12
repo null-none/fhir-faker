fd11fa5ada22ad15ef7418a39fe02d5e4125f156
Dmitry Kalinin <kalinin.mitko@gmail.com>
2021-12-12 17:23:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="fhir_faker",
@@ -11,6 +15,8 @@ setup(
     license="MIT",
     packages=["src"],
     zip_safe=False,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python",
